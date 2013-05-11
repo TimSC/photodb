ec663356d7e8031166c71955260e5e4f0b267114
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-05-11 19:16:36 +0100

    Clicking on image moves point

--- roi.php
+++ roi.php
@@ -1,26 +1,3 @@
-<html>
-<head>
-<script language="javascript" type="text/javascript">
-
-window.onload = function() {
-	document.images[0].addEventListener("mousedown", MouseDown, false);
-	document.images[0].addEventListener("mousemove", MouseMove, false);
-}
-
-function MouseDown()
-{
-	//window.alert("MouseDown");
-}
-
-function MouseMove()
-{
-	//window.alert("MouseMove");
-}
-
-
-</script>
-</head>
-<body>
 <?php
 
 require_once('photodb.php');
@@ -39,13 +16,77 @@ if(PhotoInStore($photoDb, $viewPhotoId)===0)
 	StorePhotoUrl($photoData['url'], $viewPhotoId, $photoDb);
 }
 $fina = PhotoInStore($photoDb, $viewPhotoId);
+$photoData = GetPhotoData($photoDb, $viewPhotoId);
+?>
+
+<html>
+<head>
+<script language="javascript" type="text/javascript">
+var px = 100, py = 100;
+var img, ctx;
+
+window.onload = function() {	
+	var canvas = document.getElementById('canv');
+	ctx = canvas.getContext('2d');
+
+	img = new Image();   // Create new img element
+	img.onload = function(){
+		ctx.drawImage(img,0,0);
+		DrawOverlay(ctx)
+	};
+	img.src = '<?php echo $fina;?>'; // Set source path
+
+	canvas.addEventListener("mousedown", MouseDown, false);
+	canvas.addEventListener("mousemove", MouseMove, false);
+
+	//DrawOverlay(ctx)
+}
+
+function MouseDown(e)
+{
+    if(e.offsetX) {
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+    }
+    else if(e.layerX) {
+        mouseX = e.layerX;
+        mouseY = e.layerY;
+    }
+
+	px = mouseX;
+	py = mouseY;
+	ctx.drawImage(img,0,0);
+	DrawOverlay(ctx)
+	//window.alert(px)
+	//window.alert("MouseDown");
+}
+
+function MouseMove()
+{
+	//window.alert("MouseMove");
+}
+
+function DrawOverlay(ctx)
+{
+    ctx.beginPath();
+    ctx.moveTo(px-10,py);
+    ctx.lineTo(px+10,py);
+    ctx.moveTo(px,py-10);
+    ctx.lineTo(px,py+10);
+    ctx.stroke();
+}
+
+</script>
+</head>
+<body>
+<?php
 if($fina!==0)
 {
 ?>
 
 <h1>Edit ROI</h1>
 
-<img id="foo" src="<?php echo $fina;?>" alt="Photo"/><br/>
+<canvas id="canv" style="position: relative;" width="<?php echo $photoData['width'];?>" height="<?php echo $photoData['height'];?>">Canvas not supported</canvas><br/>
 
 <?php
 }
