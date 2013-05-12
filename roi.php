361efa259273a4a4efd38e210d954b028083b42a
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-05-12 10:44:19 +0100

    Resize roi image

--- roi.php
+++ roi.php
@@ -30,11 +30,17 @@ if(isset($_POST['form-action']) && $_POST['form-action'] == "Update ROIs")
 	UpdateRois($photoDb, $viewPhotoId, json_decode($_POST['bbox']));
 }
 
-$bboxesJson = Null;
-//if(isset($_POST['bbox']))
-//	$bboxesJson = $_POST['bbox'];
 $bboxesJson = json_encode(GetRois($photoDb, $viewPhotoId));
 
+//Calculate image sizing
+$maxDimension = $photoData['width'];
+if($photoData['height']>$maxDimension)
+	$maxDimension = $photoData['height'];
+$displayRatio = 1.;
+if($maxDimension > 800)
+	$displayRatio = 800. / $maxDimension;
+$displaywidth = $photoData['width'] * $displayRatio;
+$displayheight = $photoData['height'] * $displayRatio;
 
 ?>
 
@@ -60,6 +66,8 @@ bboxes[0] = new Array(100, 100, 200, 200);
 
 var img, ctx;
 var pressed = 0, selectedBbox = -1;
+var displaywidth = <?php echo $displaywidth;?>, displayheight = <?php echo $displayheight;?>;
+var displayratio = <?php echo $displayRatio;?>;
 
 window.onload = function() {	
 	var canvas = document.getElementById('canv');
@@ -67,7 +75,7 @@ window.onload = function() {
 
 	img = new Image();   // Create new img element
 	img.onload = function(){
-		ctx.drawImage(img,0,0);
+		ctx.drawImage(img, 0, 0, displaywidth, displayheight);
 		DrawOverlay(ctx)
 	};
 	img.src = '<?php echo $fina;?>'; // Set source path
@@ -104,25 +112,25 @@ function MouseDown(e)
 	var closestDist = -1.;
 	for (i=0;i<bboxes.length;i++)
 	{
-		distA = Math.sqrt(Math.pow(bboxes[i][0] - mouseX, 2) + Math.pow(bboxes[i][1] - mouseY, 2));
+		distA = Math.sqrt(Math.pow(bboxes[i][0]*displayratio - mouseX, 2) + Math.pow(bboxes[i][1]*displayratio - mouseY, 2));
 		if(closestInd == -1 || distA < closestDist)
 		{
 			closestInd = i;
 			closestDist = distA;
 		}
-		distB = Math.sqrt(Math.pow(bboxes[i][2] - mouseX, 2) + Math.pow(bboxes[i][3] - mouseY, 2));
+		distB = Math.sqrt(Math.pow(bboxes[i][2]*displayratio - mouseX, 2) + Math.pow(bboxes[i][3]*displayratio - mouseY, 2));
 		if(closestInd == -1 || distB < closestDist)
 		{
 			closestInd = i;
 			closestDist = distB;
 		}
-		distC = Math.sqrt(Math.pow(bboxes[i][0] - mouseX, 2) + Math.pow(bboxes[i][3] - mouseY, 2));
+		distC = Math.sqrt(Math.pow(bboxes[i][0]*displayratio - mouseX, 2) + Math.pow(bboxes[i][3]*displayratio - mouseY, 2));
 		if(closestInd == -1 || distC < closestDist)
 		{
 			closestInd = i;
 			closestDist = distC;
 		}
-		distD = Math.sqrt(Math.pow(bboxes[i][2] - mouseX, 2) + Math.pow(bboxes[i][1] - mouseY, 2));
+		distD = Math.sqrt(Math.pow(bboxes[i][2]*displayratio - mouseX, 2) + Math.pow(bboxes[i][1]*displayratio - mouseY, 2));
 		if(closestInd == -1 || distD < closestDist)
 		{
 			closestInd = i;
@@ -132,9 +140,9 @@ function MouseDown(e)
 
 	//Update bounding box location
 	selectedBbox = closestInd;
-	bboxes[selectedBbox][0] = mouseX;
-	bboxes[selectedBbox][1] = mouseY;
-	ctx.drawImage(img,0,0);
+	bboxes[selectedBbox][0] = mouseX/displayratio;
+	bboxes[selectedBbox][1] = mouseY/displayratio;
+	ctx.drawImage(img, 0, 0, displaywidth, displayheight);
 	DrawOverlay(ctx)
 	var bboxFormEl = document.getElementById('form-bbox');
 	bboxFormEl.value = JSON.stringify(bboxes)
@@ -156,9 +164,9 @@ function MouseMove(e)
         mouseX = e.layerX;
         mouseY = e.layerY;
     }
-	bboxes[selectedBbox][2] = mouseX;
-	bboxes[selectedBbox][3] = mouseY;
-	ctx.drawImage(img,0,0);
+	bboxes[selectedBbox][2] = mouseX/displayratio;
+	bboxes[selectedBbox][3] = mouseY/displayratio;
+	ctx.drawImage(img, 0, 0, displaywidth, displayheight);
 	DrawOverlay(ctx)
 	var bboxFormEl = document.getElementById('form-bbox');
 	bboxFormEl.value = JSON.stringify(bboxes)
@@ -175,11 +183,11 @@ function DrawOverlay(ctx)
 	for (i=0;i<bboxes.length;i++)
 	{
 		ctx.beginPath();
-		ctx.moveTo(bboxes[i][0],bboxes[i][1]);
-		ctx.lineTo(bboxes[i][2],bboxes[i][1]);
-		ctx.lineTo(bboxes[i][2],bboxes[i][3]);
-		ctx.lineTo(bboxes[i][0],bboxes[i][3]);
-		ctx.lineTo(bboxes[i][0],bboxes[i][1]);
+		ctx.moveTo(bboxes[i][0]*displayratio,bboxes[i][1]*displayratio);
+		ctx.lineTo(bboxes[i][2]*displayratio,bboxes[i][1]*displayratio);
+		ctx.lineTo(bboxes[i][2]*displayratio,bboxes[i][3]*displayratio);
+		ctx.lineTo(bboxes[i][0]*displayratio,bboxes[i][3]*displayratio);
+		ctx.lineTo(bboxes[i][0]*displayratio,bboxes[i][1]*displayratio);
 		ctx.stroke();
 	}
 }
@@ -195,7 +203,7 @@ function NumRoisChanged()
 	while(bboxes.length < numRois)
 		bboxes.push(new Array(200,200,300,300));
 
-	ctx.drawImage(img,0,0);
+	ctx.drawImage(img, 0, 0, displaywidth, displayheight);
 	DrawOverlay(ctx)
 	var bboxFormEl = document.getElementById('form-bbox');
 	bboxFormEl.value = JSON.stringify(bboxes)
@@ -211,7 +219,7 @@ if($fina!==0)
 
 <h1>Edit ROIs</h1>
 
-<canvas id="canv" style="position: relative;" width="<?php echo $photoData['width'];?>" height="<?php echo $photoData['height'];?>">Canvas not supported</canvas><br/>
+<canvas id="canv" style="position: relative;" width="<?php echo $displaywidth;?>" height="<?php echo $displayheight;?>">Canvas not supported</canvas><br/>
 
 <p>Number of ROIs <input id="num-rois" type="text" name="num-rois" value="1"><input type="submit" value="Set" onclick="NumRoisChanged()"></p>
 
